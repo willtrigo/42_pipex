73155672fb0955c7d9ff187258feaac42ee7996b
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2024-03-17 07:08:09 -0300

    Feat: Implement handle message.  Changes to be committed: 	modified:   src/internal/ft_parse.c 	modified:   src/internal/ft_utils.c

--- src/internal/ft_parse.c
+++ src/internal/ft_parse.c
@@ -6,10 +6,11 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 02:25:03 by dande-je          #+#    #+#             */
-/*   Updated: 2024/03/16 03:26:41 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/03/17 07:05:01 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include <unistd.h>
 #include "ft_parse.h"
 #include "ft_utils.h"
@@ -18,8 +19,8 @@ void	ft_parse_arguments(int argc)
 {
 	if (argc < EXPECT_ARGC)
 		ft_handle_msg("pipex", "Too few arguments", STDOUT_FILENO, \
-			STDOUT_FILENO);
+			EXIT_FAILURE);
 	else if (argc > EXPECT_ARGC)
 		ft_handle_msg("pipex", "Too many arguments", STDOUT_FILENO, \
-			STDOUT_FILENO);
+			EXIT_FAILURE);
 }
--- src/internal/ft_utils.c
+++ src/internal/ft_utils.c
@@ -6,17 +6,25 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 03:08:08 by dande-je          #+#    #+#             */
-/*   Updated: 2024/03/16 03:21:14 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/03/17 07:02:20 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include "ft_non_standard/ft_non_standard.h"
+#include <unistd.h>
+#include "ft_non_standard/ft_sprintf.h"
+#include "ft_string.h"
 
 void	ft_handle_msg(char *process, char *msg, int fd, int exit_status)
 {
-	ft_putstr_fd(process, fd);
-	ft_putstr_fd(": ", fd);
-	ft_putendl_fd(msg, fd);
+	char	*str;
+	int		str_len;
+
+	str = NULL;
+	if (!ft_strncmp(process, "pipex", ft_str_len("pipex")))
+		str_len = ft_sprintf(&str, "pipex: %s\n", msg);
+	else
+		str_len = ft_sprintf(&str, "pipex: %s: %s\n", process, msg);
+	write(fd, str, str_len);
 	exit (exit_status);
 }
